e60304a9e6f66ab0695d05bb8674b0e8c279f404
~RP <radhepipaliya13112004@gmail.com>
2026-06-29 09:28:31 +0530

    add update patient functionality and exception handling for patient not found

--- patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
+++ patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
@@ -3,12 +3,14 @@ package com.pm.patientservice.controller;
 
 import com.pm.patientservice.dto.PatientRequestDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.model.Patient;
 import com.pm.patientservice.service.PatientService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/patients") //http://localhost:4000/patients
@@ -33,4 +35,13 @@ public class PatientController {
 
         return ResponseEntity.ok().body(patientResponseDTO);
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<PatientResponseDTO> UpdatePatient(@PathVariable UUID id, @RequestBody PatientRequestDTO patientRequestDTO){
+
+        PatientResponseDTO patientResponseDTO = patientService.updatePatient(id,patientRequestDTO);
+
+        return ResponseEntity.ok().body(patientResponseDTO);
+    }
+
 }
